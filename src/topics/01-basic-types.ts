a30a9d1c72f99e022e93730a6a3bcb9c24556b70
Cristobal Rosa <crisrogar@gmail.com>
2024-11-14 13:49:06 +0100

    update 01-basic-type.ts

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,21 @@
+// number, string, boolean, date, any
+
+//#region NUMBER 
+console.log('***************************')
+console.log('***********NUMBER**********')
+console.log('***************************')
+
+const age = 15;
+let weight = 70.5;
+weight = 85;
+let weight2 :number;
+weight2 = 74;
+
+console.log(age);
+console.log(weight);
+console.log(weight2);
+
+//#endregion
+
+
+export {};
