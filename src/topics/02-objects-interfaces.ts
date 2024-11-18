f9b9e3e3021db0ca83a2f353c8e3250a3810e7c7
Cristobal Rosa <crisrogar@gmail.com>
2024-11-18 11:17:10 +0100

    update 02-objects-interfaces.ts

--- src/topics/02-objects-interfaces.ts
+++ src/topics/02-objects-interfaces.ts
@@ -6,6 +6,11 @@ interface IPerson {
     age :number
 }
 
+interface IStudent extends IPerson {
+    career :string;
+    university: string;
+}
+
 // OBJECTS
 
 const juan :IPerson = {
@@ -14,6 +19,16 @@ const juan :IPerson = {
     age :42
 }
 
+const studentPercy :IStudent = {
+    name: 'Percy',
+    lastName: 'García Kori',
+    age :24,
+    career: 'Ingeniería informática',
+    university: 'Universidad de Córdoba'
+
+}
+
 console.log('Datos de Juan: ', juan);
+console.log('Datos del estudiante Percy: ', studentPercy);
 
 export{};
