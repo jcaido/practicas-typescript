8ad1c9828558a68548fbcadefe511a9704f3ed95
Cristobal Rosa <crisrogar@gmail.com>
2024-11-19 11:06:33 +0100

    update 04-functions-methods.ts

--- src/topics/04-functions-methods.ts
+++ src/topics/04-functions-methods.ts
@@ -8,6 +8,11 @@ function showMessages() :void {
     console.log('Hello coders');
 }
 
+const greet = (name :string) :void => {
+    console.log(`Hello ${name}`);
+}
+
 showMessages();
+greet('Cristóbal');
 
 export {}
