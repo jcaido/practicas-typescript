8b8cb935b85795d4dd5eaf9668598c1fb99c1237
Cristobal Rosa <crisrogar@gmail.com>
2024-11-19 10:11:47 +0100

    update 03-arrays.ts

--- src/topics/03-arrays.ts
+++ src/topics/03-arrays.ts
@@ -1,3 +1,37 @@
+const element = [2, 'sdffdd', false, {name: 'julio'}];
+console.log('Elementos: ', element);
 
+const messages :string[] = ['Hello', 'Coders'];
+console.log('Messages: ', messages);
+
+
+//array de objetos
+
+interface IPet {
+    name: string;
+    age: string;
+    species: string;
+    isFemale: boolean;
+    vaccines: string[];
+}
+
+const petFirullay :IPet = {
+    name: 'Firulay',
+    age: '15 años',
+    species: 'Perro',
+    isFemale: false,
+    vaccines: ['Rabia']
+};
+
+const petKitty:IPet = {
+    name: 'Kitty',
+    age: '8 años',
+    species: 'Gato',
+    isFemale: true,
+    vaccines: ['Rabia', 'Distemper']
+}
+
+const pets :IPet[] = [petFirullay, petKitty];
+console.log('Mis mascotas: ', pets);
 
 export{}
