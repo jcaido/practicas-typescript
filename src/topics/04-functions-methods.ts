6218ff62e61a98f8e6563a55ab80b6a9339e9ba9
Cristobal Rosa <crisrogar@gmail.com>
2024-11-19 11:02:33 +0100

    create 04-functions-methods.ts

---
+++ src/topics/04-functions-methods.ts
@@ -0,0 +1,13 @@
+//METODOS
+
+console.log('***************************')
+console.log('***********METHODS**********')
+console.log('***************************')
+
+function showMessages() :void {
+    console.log('Hello coders');
+}
+
+showMessages();
+
+export {}
