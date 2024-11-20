a6b4c7e0ebdbcc368a5d2399b13d5697b8596197
Cristobal Rosa <crisrogar@gmail.com>
2024-11-20 11:04:51 +0100

    update 05-destructuring-arrays-objects.ts

--- src/topics/05-destructuring-arrays-obects.ts
+++ src/topics/05-destructuring-arrays-obects.ts
@@ -23,8 +23,24 @@ const customer :ICostumer = {
 console.log('Nombre: ', customer.name);
 console.log('Apellidos: ', customer.lastName);
 
-const {name, lastName} = customer;
+const {name, lastName, address: {city}} = customer;
 console.log('Nombre: ', name);
 console.log('Apellidos: ', lastName);
+console.log('City: ', city);
+
+console.log('***************************')
+console.log('***********ARRAYS**********')
+console.log('***************************')
+
+const pokemons :string[] = ['Pikachu', 'Charizard', 'Chikorita'];
+console.log('Pokemon 2: ', pokemons[1]);
+
+const [poke1, poke2] = pokemons;
+console.log('Pokemon 1: ', poke1);
+console.log('Pokemon 2: ', poke2);
+
+const [, , poke3] = pokemons;
+console.log('Pokemon 3: ', poke3);
+
 
 export {}
