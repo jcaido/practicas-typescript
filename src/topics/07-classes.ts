5747c4a11f3ef52501ebbf5ff6acc5827424c979
Cristobal Rosa <crisrogar@gmail.com>
2024-11-22 10:44:09 +0100

    update 07-classes.ts

--- src/topics/07-classes.ts
+++ src/topics/07-classes.ts
@@ -1,10 +1,16 @@
 class Person {
     name: string;
-    lastNmae: string;
+    public lastNmae: string;
+    private age: number
 
-    constructor(name: string, lastName: string) {
+    constructor(name: string, lastName: string, age: number) {
         this.name = name;
         this.lastNmae = lastName;
+        this.age = age;
+    }
+
+    getAge() :number {
+        return this.age
     }
 
     walk() :void {
@@ -12,7 +18,8 @@ class Person {
     }
 }
 
-const juan = new Person('Juan', 'Pérez Vargas');
+const juan = new Person('Juan', 'Pérez Vargas', 15);
 console.log('Instancia (Person) "JUAN": ', juan);
+console.log('edad de JUAN: ', juan.getAge());
 
 export {}
