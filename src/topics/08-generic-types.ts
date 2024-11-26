0c279ea4a31390dba9dbba7294fd001ee3c8fa7e
Cristobal Rosa <crisrogar@gmail.com>
2024-11-26 10:54:46 +0100

    update 08-generic-types.ts

--- src/topics/08-generic-types.ts
+++ src/topics/08-generic-types.ts
@@ -1,3 +1,5 @@
+import { ICustomer } from "./utils/customer-util";
+
 const array1 = [1, 5, 2, 9, 1];
 const array2 = ['a', 'b', 'c'];
 
@@ -20,5 +22,67 @@ const getFirstItem = <T>(array: T[]) => {
 
 console.log(getFirstItem<string>(array2));
 console.log(getFirstItem<number>(array1));
+
+//Recibimos un modelo del backend en formato JSON y podemos pasarlo a TS con la extension JSON to TS de Visual Studio Code (Copy-paste)
+
+/*
+
+{
+    "code": "INFO001",
+    "message": "Sin errores al procesar",
+    "data": {
+        "name": "juan",
+        "lastName": "Sosa Vega"
+    }
+}
+
+{
+    "code": "INFO001",
+    "message": "Sin errores al procesar",
+    "data": [
+        {
+            "name": "juan",
+            "lastName": "Sosa Vega"
+        },
+        {
+            "name": "vanessa",
+            "lastName": "Campos Torres"
+        }
+    ]
+}
+
+{
+    "code": "INFO001",
+    "message": "Sin errores al procesar",
+    "data": true
+}
+
+El backend envía al frontend tres modelos JSON en los que solo varia el atributo data. Podemos utilizar genéricos
+*/
+
+interface IResponse<T> {
+  code: string;
+  message: string;
+  data: T;
+}
+
+const customer: ICustomer = {
+    name: 'Dina',
+    lastName: 'Boluarte García',
+    email: 'dina@gmail.com',
+    address: {
+        description: 'Avda. Cordoba',
+        city: 'Monturque',
+        country: 'España'
+    }
+}
+
+const responseObjet: IResponse<ICustomer> = {
+    code: '23eer',
+    message: 'Proceso iniciado',
+    data: customer
+}
+
+console.log('Response object: ', responseObjet);
 
 export {} 
